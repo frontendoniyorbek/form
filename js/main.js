7ca3cd53a46c6e1ef10e503fcf62ce521be28a6d
frontendoniyorbek <tursunovdoniyorbek077@gmail.com>
2022-05-19 22:39:10 -0700

    Add form

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// const elSiteThemToggler =document.querySelector('.site-header-light');
+// const elSiteThemToggle =document.querySelector('.site-header-dark');
+
+// elSiteThemToggler.addEventListener('click', function () {
+//   document.body.classList.toggle('dark-mode');
+// });
